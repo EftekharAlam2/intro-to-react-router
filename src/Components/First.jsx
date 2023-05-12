4f0f2c0b1edfa2dac36374500aff19455f296dd6
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-12 18:36:58 +0600

    friends page added

---
+++ src/Components/First.jsx
@@ -0,0 +1,9 @@
+const First = () => {
+  return (
+    <div>
+      <h3>This is the default first page</h3>
+    </div>
+  );
+};
+
+export default First;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,25 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Components/Home";
 import ErrorElement from "./Components/ErrorElement";
+import First from "./Components/First";
+import Friends from "./Components/Friends/Friends";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
     errorElement: <ErrorElement></ErrorElement>,
+    children: [
+      {
+        path: "/",
+        element: <First></First>,
+      },
+      {
+        path: "friends",
+        element: <Friends></Friends>,
+        loader: () => fetch("https://jsonplaceholder.typicode.com/users"),
+      },
+    ],
   },
 ]);
 
